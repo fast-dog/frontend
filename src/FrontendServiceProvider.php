d76cd87148437cf43d56d708d7a9444d97b69b8b
Andrey Martynov <d.g.dev482@gmail.com>
2019-05-10 13:25:25 +0500

    Change readme

---
+++ src/FrontendServiceProvider.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace FastDog\Frontend;
+
+
+use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
+
+/**
+ * Class FrontendServiceProvider
+ *
+ * @package FastDog\Frontend
+ * @version 0.1.0
+ * @author Андрей Мартынов <d.g.dev482@gmail.com>
+ */
+class FrontendServiceProvider
+{
+    const NAME = 'frontend';
+
+    /**
+     * php composer.phar update fast_dog_core:dev-master --prefer-source
+     *
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        $this->handleViews();
+    }
+
+
+    /**
+     * Определение представлении пакета (шаблонов по умолчанию)
+     */
+    private function handleViews(): void
+    {
+        $this->loadViewsFrom(__DIR__ . DIRECTORY_SEPARATOR . '..' .
+            DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR, self::NAME);
+
+        $this->publishes([__DIR__ . DIRECTORY_SEPARATOR . '..' .
+        DIRECTORY_SEPARATOR . 'dist' . DIRECTORY_SEPARATOR => base_path('resources/views/vendor/fast_dog/' . self::NAME)]);
+
+        $this->publishes([
+            __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'assets' => public_path('vendor/fast_dog'),
+        ], 'public');
+    }
+}
