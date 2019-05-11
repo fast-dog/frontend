c6aa6abd2242d1d69c25b9158a295401a9abc523
Andrey Martynov <d.g.dev482@gmail.com>
2019-05-11 20:05:02 +0500

    доработано боковое меню

--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -4,11 +4,16 @@
         <div class="menu_section">
             <ul class="nav side-menu">
                 <li v-for="item in $store.getters.getMainMenu">
-                    <a><i class="fa fa-home"></i> {{item.name}} <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                        <li><a href="index.html">Dashboard</a></li>
-                        <li><a href="index2.html">Dashboard2</a></li>
-                        <li><a href="index3.html">Dashboard3</a></li>
+                    <a>
+                        <i :class="'fa ' + item.icon"></i> {{item.name}}
+                        <span class="fa fa-chevron-down" v-if="item.children.length > 0"></span>
+                    </a>
+                    <ul class="nav child_menu" v-if="item.children.length > 0">
+                        <li v-for="child in item.children">
+                            <router-link :to="child.route">
+                                <i :class="'fa ' + child.icon" v-if="child.icon"></i> {{child.name}}
+                            </router-link>
+                        </li>
                     </ul>
                 </li>
             </ul>
@@ -18,8 +23,6 @@
     <!-- /sidebar menu -->
 </template>
 <script lang="ts">
-
-
     import axios from 'axios'
     import {Component, Provide, Vue, Watch} from 'vue-property-decorator'
 
@@ -38,11 +41,23 @@
 
         @Watch('$store.getters.getMainMenu')
         onSetMainMenu(items, oldItems) {
-            console.log(items);
+            let me = this;
+
+            me.$nextTick(function () {
+                me.init_sidebar();
+            })
         }
 
         mounted(): void {
+            let me = this;
+            me.$store.dispatch('setMainMenu');
+            // /Sidebar
+            document.body.classList.add('nav-md', 'footer_fixed');
 
+        }
+
+        // Sidebar
+        init_sidebar() {
             let CURRENT_URL = window.location.href.split('#')[0].split('?')[0],
                 $BODY = $('body'),
                 $MENU_TOGGLE = $('#menu_toggle'),
@@ -51,109 +66,97 @@
                 $LEFT_COL = $('.left_col'),
                 $RIGHT_COL = $('.right_col'),
                 $NAV_MENU = $('.nav_menu'),
-                $FOOTER = $('footer'),
-                me = this;
+                $FOOTER = $('footer');
+            // TODO: This is some kind of easy fix, maybe we can improve this
+            var setContentHeight = function () {
+                // reset height
+                $RIGHT_COL.css('min-height', $(window).height());
 
-            me.$store.dispatch('setMainMenu');
+                var bodyHeight = $BODY.outerHeight(),
+                    footerHeight = $BODY.hasClass('footer_fixed') ? -10 : $FOOTER.height(),
+                    leftColHeight = $LEFT_COL.eq(1).height() + $SIDEBAR_FOOTER.height(),
+                    contentHeight = bodyHeight < leftColHeight ? leftColHeight : bodyHeight;
 
-            // Sidebar
-            function init_sidebar() {
-                // TODO: This is some kind of easy fix, maybe we can improve this
-                var setContentHeight = function () {
-                    // reset height
-                    $RIGHT_COL.css('min-height', $(window).height());
+                // normalize content
+                contentHeight -= $NAV_MENU.height() + footerHeight;
 
-                    var bodyHeight = $BODY.outerHeight(),
-                        footerHeight = $BODY.hasClass('footer_fixed') ? -10 : $FOOTER.height(),
-                        leftColHeight = $LEFT_COL.eq(1).height() + $SIDEBAR_FOOTER.height(),
-                        contentHeight = bodyHeight < leftColHeight ? leftColHeight : bodyHeight;
+                $RIGHT_COL.css('min-height', contentHeight);
+            };
 
-                    // normalize content
-                    contentHeight -= $NAV_MENU.height() + footerHeight;
+            $SIDEBAR_MENU.find('a').on('click', function (ev) {
+                console.log('clicked - sidebar_menu');
+                var $li = $(this).parent();
 
-                    $RIGHT_COL.css('min-height', contentHeight);
-                };
-
-                $SIDEBAR_MENU.find('a').on('click', function (ev) {
-                    console.log('clicked - sidebar_menu');
-                    var $li = $(this).parent();
-
-                    if ($li.is('.active')) {
-                        $li.removeClass('active active-sm');
-                        $('ul:first', $li).slideUp(function () {
-                            setContentHeight();
-                        });
+                if ($li.is('.active')) {
+                    $li.removeClass('active active-sm');
+                    $('ul:first', $li).slideUp(function () {
+                        setContentHeight();
+                    });
+                } else {
+                    // prevent closing menu if we are on child menu
+                    if (!$li.parent().is('.child_menu')) {
+                        $SIDEBAR_MENU.find('li').removeClass('active active-sm');
+                        $SIDEBAR_MENU.find('li ul').slideUp();
                     } else {
-                        // prevent closing menu if we are on child menu
-                        if (!$li.parent().is('.child_menu')) {
+                        if ($BODY.is('.nav-sm')) {
                             $SIDEBAR_MENU.find('li').removeClass('active active-sm');
                             $SIDEBAR_MENU.find('li ul').slideUp();
-                        } else {
-                            if ($BODY.is('.nav-sm')) {
-                                $SIDEBAR_MENU.find('li').removeClass('active active-sm');
-                                $SIDEBAR_MENU.find('li ul').slideUp();
-                            }
                         }
-                        $li.addClass('active');
-
-                        $('ul:first', $li).slideDown(function () {
-                            setContentHeight();
-                        });
                     }
-                });
+                    $li.addClass('active');
 
-                // toggle small or large menu
-                $MENU_TOGGLE.on('click', function () {
-                    console.log('clicked - menu toggle');
-
-                    if ($BODY.hasClass('nav-md')) {
-                        $SIDEBAR_MENU.find('li.active ul').hide();
-                        $SIDEBAR_MENU.find('li.active').addClass('active-sm').removeClass('active');
-                    } else {
-                        $SIDEBAR_MENU.find('li.active-sm ul').show();
-                        $SIDEBAR_MENU.find('li.active-sm').addClass('active').removeClass('active-sm');
-                    }
-
-                    $BODY.toggleClass('nav-md nav-sm');
-
-                    setContentHeight();
-
-                    $('.dataTable').each(function () {
-                        $(this).dataTable().fnDraw();
+                    $('ul:first', $li).slideDown(function () {
+                        setContentHeight();
                     });
-                });
+                }
+            });
 
-                // check active menu
-                $SIDEBAR_MENU.find('a[href="' + CURRENT_URL + '"]').parent('li').addClass('current-page');
+            // toggle small or large menu
+            $MENU_TOGGLE.on('click', function () {
+                console.log('clicked - menu toggle');
 
-                $SIDEBAR_MENU.find('a').filter(function () {
-                    return this.href == CURRENT_URL;
-                }).parent('li').addClass('current-page').parents('ul').slideDown(function () {
-                    setContentHeight();
-                }).parent().addClass('active');
+                if ($BODY.hasClass('nav-md')) {
+                    $SIDEBAR_MENU.find('li.active ul').hide();
+                    $SIDEBAR_MENU.find('li.active').addClass('active-sm').removeClass('active');
+                } else {
+                    $SIDEBAR_MENU.find('li.active-sm ul').show();
+                    $SIDEBAR_MENU.find('li.active-sm').addClass('active').removeClass('active-sm');
+                }
 
-                // recompute content when resizing
-                $(window).smartresize(function () {
-                    setContentHeight();
-                });
+                $BODY.toggleClass('nav-md nav-sm');
 
                 setContentHeight();
 
-                // fixed sidebar
-                if ($.fn.mCustomScrollbar) {
-                    $('.menu_fixed').mCustomScrollbar({
-                        autoHideScrollbar: true,
-                        theme: 'minimal',
-                        mouseWheel: {preventDefault: true}
-                    });
-                }
+                $('.dataTable').each(function () {
+                    $(this).dataTable().fnDraw();
+                });
+            });
+
+            // check active menu
+            $SIDEBAR_MENU.find('a[href="' + CURRENT_URL + '"]').parent('li').addClass('current-page');
+
+            $SIDEBAR_MENU.find('a').filter(function () {
+                return this.href == CURRENT_URL;
+            }).parent('li').addClass('current-page').parents('ul').slideDown(function () {
+                setContentHeight();
+            }).parent().addClass('active');
+
+            // recompute content when resizing
+            $(window).smartresize(function () {
+                setContentHeight();
+            });
+
+            setContentHeight();
+
+            // fixed sidebar
+            if ($.fn.mCustomScrollbar) {
+                $('.menu_fixed').mCustomScrollbar({
+                    autoHideScrollbar: true,
+                    theme: 'minimal',
+                    mouseWheel: {preventDefault: true}
+                });
             }
-
-            // /Sidebar
-            document.body.classList.add('nav-md', 'footer_fixed');
-            init_sidebar();
         }
-
     }
 
 </script>
