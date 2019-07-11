bd0f326a5285565555e6127632631f3326e0ec1f
Andrey Martynov <d.g.dev482@gmail.com>
2019-07-11 09:25:49 +0500

    move ckeditor

--- src/FrontendServiceProvider.php
+++ src/FrontendServiceProvider.php
@@ -47,5 +47,8 @@ class FrontendServiceProvider extends LaravelServiceProvider
         $this->publishes([__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR .
         'assets' =>
             public_path('vendor/fast_dog/' . self::NAME)], 'public');
+
+        $this->publishes([__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'ckeditor' . DIRECTORY_SEPARATOR .
+        'assets' => public_path('ckeditor/')], 'public');
     }
 }
