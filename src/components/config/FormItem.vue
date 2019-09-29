21bcc0030c15d46f90692ad8f129d655c689f7ff
dg <d.g.dev482@gmail.com>
2019-09-30 00:35:07 +0500

    dev new form manager

---
+++ src/components/config/FormItem.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="row">
+    <div class="col-lg-12">
+      <div class="x_panel">
+        <div class="x_title">form name</div>
+        <div class="x_content">
+          <div class="tabs-container">
+            <ul class="nav nav-tabs bar_tabs">
+              <li><a href="#">tab 1</a></li>
+            </ul>
+            <div class="tab-content">
+              <div class="panel-body">
+                <div class="col-sm-9">
+                  <div class="col-sm-6">
+                    <div class="fields"></div>
+                  </div>
+                  <div class="col-sm-6">
+                    <div class="fields"></div>
+                  </div>
+                  <div class="col-sm-12">
+                    <div class="fields"></div>
+                  </div>
+                </div>
+                <div class="col-sm-3">
+                  <div class="fields"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue';
+    import {Component, Provide, Watch} from 'vue-property-decorator'
+
+
+    declare let $: any;
+
+    @Component({
+        name: 'FormItem',
+        components: {}
+    })
+
+    export default class FormItem extends Vue {
+
+    }
+</script>
+
+<style scoped lang="scss">
+.fields{
+  min-height: 60px;
+  border: 1px dashed #1c84c6;
+  margin: 2px;
+  background: rgba(0, 136, 249, 0.09);
+}
+</style>
--- src/router.ts
+++ src/router.ts
@@ -16,17 +16,18 @@ import DomainItems from '@/components/config/DomainItems.vue';
 import DomainItem from '@/components/config/DomainItem.vue';
 import ComponentsItems from '@/components/config/ComponentsItems.vue';
 import ComponentsItem from '@/components/config/ComponentsItem.vue';
-import EmailsItems from "@/components/config/EmailsItems.vue";
-import EmailsItem from "@/components/config/EmailsItem.vue";
-import LocalizationItems from "@/components/config/LocalizationItems.vue";
-import LocalizationItem from "@/components/config/LocalizationItem.vue";
+import EmailsItems from '@/components/config/EmailsItems.vue';
+import EmailsItem from '@/components/config/EmailsItem.vue';
+import LocalizationItems from '@/components/config/LocalizationItems.vue';
+import LocalizationItem from '@/components/config/LocalizationItem.vue';
 import HelpItems from './components/config/HelpItems.vue';
 import HelpItem from './components/config/HelpItem.vue';
-import Menu from "@/components/menu/Menu.vue";
-import MenuIndex from "@/components/menu/MenuIndex.vue";
+import Menu from '@/components/menu/Menu.vue';
+import MenuIndex from '@/components/menu/MenuIndex.vue';
 import MenuItem from './components/menu/MenuItem.vue';
-import MenuItems from "@/components/menu/MenuItems.vue";
-import PageItem from "@/components/menu/PageItem.vue";
+import MenuItems from '@/components/menu/MenuItems.vue';
+import PageItem from '@/components/menu/PageItem.vue';
+import FormItem from './components/config/FormItem.vue';
 
 declare let window: any;
 
@@ -38,226 +39,231 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 let token: any = document.head.querySelector('meta[name="csrf-token"]');
 
 if (token) {
-    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+  window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
 } else {
-    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+  console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
 
 Vue.use(Router);
 
 export default new Router({
-    // mode: 'history',
-    base: process.env.BASE_URL,
-    routes: [
+  // mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: '/users/',
+      component: Users,
+      children: [
         {
-            path: '/',
-            name: 'home',
-            component: Home,
+          path: 'items',
+          name: 'users',
+          component: UsersItems
         },
         {
-            path: '/users/',
-            component: Users,
-            children: [
-                {
-                    path: 'items',
-                    name: 'users',
-                    component: UsersItems
-                },
-                {
-                    path: 'items/page/:page',
-                    name: 'users_list_page',
-                    component: UsersItems
-                },
-                {
-                    path: 'item/:id',
-                    name: 'user_profile',
-                    component: UsersItem
-                },
-                {
-                    path: 'subscribe',
-                    name: 'user_subscribe',
-                    component: UsersSubscribeItems
-                },
-                {
-                    path: 'subscribe/page/:page',
-                    name: 'users_subscribe_list_page',
-                    component: UsersSubscribeItems
-                },
-                {
-                    path: 'mailing',
-                    name: 'mailing',
-                    component: UsersMailing
-                },
-                {
-                    path: 'mailing/page/:page',
-                    name: 'mailing_page',
-                    component: UsersMailing
-                },
-                {
-                    path: 'mailing/:id',
-                    name: 'mailing_item',
-                    component: UsersMailingItem
-                },
-                {
-                    path: 'mailing-templates',
-                    name: 'mailing_templates_items',
-                    component: UsersMailingTemplateItems
-                },
-                {
-                    path: 'mailing-templates/:id',
-                    name: 'mailing_templates_item',
-                    component: UsersMailingTemplateItem
-                },
-                {
-                    path: 'mailing-status',
-                    name: 'mailing_status',
-                    component: UsersMailingStatus
-                },
-                {
-                    path: 'configuration',
-                    name: 'user_configuration',
-                    component: UsersConfiguration
-                },
-            ]
+          path: 'items/page/:page',
+          name: 'users_list_page',
+          component: UsersItems
         },
         {
-            path: '/configuration/',
-            component: Config,
-            children: [
-                {
-                    path: 'domain',
-                    name: 'domain_items',
-                    component: DomainItems
-                },
-                {
-                    path: 'domain/page/:page',
-                    name: 'domain_items_page',
-                    component: DomainItems
-                },
-                {
-                    path: 'domain/:id',
-                    name: 'domain_item',
-                    component: DomainItem
-                },
-                {
-                    path: 'components',
-                    name: 'component_items',
-                    component: ComponentsItems
-                },
-                {
-                    path: 'components/page/:page',
-                    name: 'component_items_page',
-                    component: ComponentsItems
-                },
-                {
-                    path: 'components/:id',
-                    name: 'component_item',
-                    component: ComponentsItem
-                },
-                {
-                    path: 'emails',
-                    name: 'emails_items',
-                    component: EmailsItems
-                },
-                {
-                    path: 'emails/page/:page',
-                    name: 'emails_items_page',
-                    component: EmailsItems
-                },
-                {
-                    path: 'emails/:id',
-                    name: 'emails_item',
-                    component: EmailsItem
-                },
-                {
-                    path: 'localization',
-                    name: 'localization_items',
-                    component: LocalizationItems
-                },
-                {
-                    path: 'localization/page/:page',
-                    name: 'localization_items_page',
-                    component: LocalizationItems
-                },
-                {
-                    path: 'localization/:id',
-                    name: 'localization_item',
-                    component: LocalizationItem
-                },
-                {
-                    path: 'help',
-                    name: 'help_items',
-                    component: HelpItems
-                },
-                {
-                    path: 'help/page/:page',
-                    name: 'help_items_page',
-                    component: HelpItems
-                },
-                {
-                    path: 'help/:id',
-                    name: 'help_item',
-                    component: HelpItem
-                },
-            ]
+          path: 'item/:id',
+          name: 'user_profile',
+          component: UsersItem
         },
         {
-            path: '/menu/',
-            component: Menu,
-            children: [
-                // {
-                //     path: 'configuration',
-                //     name: 'menu_info',
-                //     component: MenuInfo
-                // },
-                {
-                    path: 'index',
-                    name: 'menu',
-                    component: MenuIndex
-                },
-                {
-                    path: 'new/:parent_id',
-                    name: 'menu_create_children',
-                    component: MenuItem
-                },
-                {
-                    path: 'new',
-                    name: 'menu_create',
-                    component: MenuItem
-                },
-                {
-                    path: 'item/:id',
-                    name: 'menu_item',
-                    component: MenuItem
-                },
-                {
-                    path: 'list/:id',
-                    name: 'menu_items',
-                    component: MenuItems
-                },
-                {
-                    path: 'list/:id',
-                    name: 'menu_items_child',
-                    component: MenuItems
-                },
-                // {
-                //     path: 'diagnostic',
-                //     name: 'menu_diagnostic',
-                //     component: MenuDiagnostic
-                // },
+          path: 'subscribe',
+          name: 'user_subscribe',
+          component: UsersSubscribeItems
+        },
+        {
+          path: 'subscribe/page/:page',
+          name: 'users_subscribe_list_page',
+          component: UsersSubscribeItems
+        },
+        {
+          path: 'mailing',
+          name: 'mailing',
+          component: UsersMailing
+        },
+        {
+          path: 'mailing/page/:page',
+          name: 'mailing_page',
+          component: UsersMailing
+        },
+        {
+          path: 'mailing/:id',
+          name: 'mailing_item',
+          component: UsersMailingItem
+        },
+        {
+          path: 'mailing-templates',
+          name: 'mailing_templates_items',
+          component: UsersMailingTemplateItems
+        },
+        {
+          path: 'mailing-templates/:id',
+          name: 'mailing_templates_item',
+          component: UsersMailingTemplateItem
+        },
+        {
+          path: 'mailing-status',
+          name: 'mailing_status',
+          component: UsersMailingStatus
+        },
+        {
+          path: 'configuration',
+          name: 'user_configuration',
+          component: UsersConfiguration
+        },
+      ]
+    },
+    {
+      path: '/configuration/',
+      component: Config,
+      children: [
+        {
+          path: 'domain',
+          name: 'domain_items',
+          component: DomainItems
+        },
+        {
+          path: 'domain/page/:page',
+          name: 'domain_items_page',
+          component: DomainItems
+        },
+        {
+          path: 'domain/:id',
+          name: 'domain_item',
+          component: DomainItem
+        },
+        {
+          path: 'components',
+          name: 'component_items',
+          component: ComponentsItems
+        },
+        {
+          path: 'components/page/:page',
+          name: 'component_items_page',
+          component: ComponentsItems
+        },
+        {
+          path: 'components/:id',
+          name: 'component_item',
+          component: ComponentsItem
+        },
+        {
+          path: 'emails',
+          name: 'emails_items',
+          component: EmailsItems
+        },
+        {
+          path: 'emails/page/:page',
+          name: 'emails_items_page',
+          component: EmailsItems
+        },
+        {
+          path: 'emails/:id',
+          name: 'emails_item',
+          component: EmailsItem
+        },
+        {
+          path: 'localization',
+          name: 'localization_items',
+          component: LocalizationItems
+        },
+        {
+          path: 'localization/page/:page',
+          name: 'localization_items_page',
+          component: LocalizationItems
+        },
+        {
+          path: 'localization/:id',
+          name: 'localization_item',
+          component: LocalizationItem
+        },
+        {
+          path: 'help',
+          name: 'help_items',
+          component: HelpItems
+        },
+        {
+          path: 'help/page/:page',
+          name: 'help_items_page',
+          component: HelpItems
+        },
+        {
+          path: 'help/:id',
+          name: 'help_item',
+          component: HelpItem
+        },
+        {
+          path: 'form/:id',
+          name: 'form_item',
+          component: FormItem
+        },
+      ]
+    },
+    {
+      path: '/menu/',
+      component: Menu,
+      children: [
+        // {
+        //     path: 'configuration',
+        //     name: 'menu_info',
+        //     component: MenuInfo
+        // },
+        {
+          path: 'index',
+          name: 'menu',
+          component: MenuIndex
+        },
+        {
+          path: 'new/:parent_id',
+          name: 'menu_create_children',
+          component: MenuItem
+        },
+        {
+          path: 'new',
+          name: 'menu_create',
+          component: MenuItem
+        },
+        {
+          path: 'item/:id',
+          name: 'menu_item',
+          component: MenuItem
+        },
+        {
+          path: 'list/:id',
+          name: 'menu_items',
+          component: MenuItems
+        },
+        {
+          path: 'list/:id',
+          name: 'menu_items_child',
+          component: MenuItems
+        },
+        // {
+        //     path: 'diagnostic',
+        //     name: 'menu_diagnostic',
+        //     component: MenuDiagnostic
+        // },
 
 
-                // {
-                //     path: 'sitemap',
-                //     name: 'sitemap',
-                //     component: Sitemaps
-                // }
-                {
-                    path: 'page/:id',
-                    name: 'page_item',
-                    component: PageItem
-                },
-            ]
+        // {
+        //     path: 'sitemap',
+        //     name: 'sitemap',
+        //     component: Sitemaps
+        // }
+        {
+          path: 'page/:id',
+          name: 'page_item',
+          component: PageItem
         },
-    ]
+      ]
+    },
+  ]
 })
