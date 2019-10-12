f9fa39abe6f67b4d307910f946b8e49a802e5f48
dg <d.g.dev482@gmail.com>
2019-10-12 10:56:19 +0500

    new build

--- src/components/media/MediaIndex.vue
+++ src/components/media/MediaIndex.vue
@@ -1,8 +1,11 @@
 <template>
-  <iframe frameborder="0" :src="getElfindeerRoute()"
-          border="0"
-          height="620" width="100%"
-          noresize="noresize"></iframe>
+  <div>
+    <iframe frameborder="0" :src="getElfindeerRoute()"
+            border="0"
+            height="620" width="100%"
+            noresize="noresize"></iframe>
+  </div>
+
 </template>
 
 
@@ -21,8 +24,16 @@
 
     export default class MediaIndex extends Vue {
 
+        mounted(): void {
+            let me = this;
+            me.$store.dispatch('setBreadcrumbs', {
+                items: [{}],
+                page_title: 'Обзор файлов '
+            });
+        }
+
         getElfindeerRoute(): string {
-            return window.location.protocol + '//' + window.location.hostname + '/elfinder/popup/image';
+            return window.location.protocol + '//' + window.location.hostname + '/elfinder/popup/ckeditor';
         }
     }
 </script>
